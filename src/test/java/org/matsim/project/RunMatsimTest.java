8adca7b212f4a1fd6996c37240dfde8632ec599b
Marc <dziakowski@vsp.tu-berlin.de>
2019-12-02 15:02:47 +0100

    selectedPlans
    Signed-off-by: Marc <dziakowski@vsp.tu-berlin.de>

--- src/test/java/org/matsim/project/RunMatsimTest.java
+++ src/test/java/org/matsim/project/RunMatsimTest.java
@@ -22,7 +22,13 @@ import org.apache.log4j.Logger;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.matsim.core.utils.geometry.geotools.MGC;
+import org.matsim.core.utils.gis.PolygonFeatureFactory;
 import org.matsim.testcases.MatsimTestUtils;
+import org.opengis.feature.simple.SimpleFeature;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 /**
  * @author nagel
@@ -34,12 +40,18 @@ public class RunMatsimTest{
 
 	@Test
 	public final void test() {
+		Collection<SimpleFeature> features = new ArrayList<>();
+		PolygonFeatureFactory polygonFeatureFactory = new PolygonFeatureFactory.Builder()
+				.setCrs(MGC.getCRS("DHDN_GK4")).setName("Zone A")
+				.addAttribute("Id", String.class)
+				.create();
 		try {
 			String [] args = {"scenarios/equil/config.xml",
 				  "--config:controler.outputDirectory", utils.getOutputDirectory(),
 				  "--config:controler.lastIteration=1",
 				  "--config:controler.writeEventsInterval=1"
 			} ;
+			Assert.fail();
 			RunMatsim.main( args ) ;
 		} catch ( Exception ee ) {
 			Logger.getLogger(this.getClass()).fatal("there was an exception: \n" + ee ) ;
